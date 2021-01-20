3a90052a92dcfa544689ec12223b1cdb62af0f60
“xj.tang” <xj.tang@wanwuyunlian.com>
2021-01-20 22:57:52 +0800

    feat: HMR

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import _ from "lodash";
-import printMe from "./print.js";
+import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
-  var element = document.createElement("div");
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
 
-  var btn = document.createElement("button");
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  btn.innerHTML = "Click me and check the console!";
+  btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
 
   element.appendChild(btn);
@@ -17,3 +16,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: "./src/index.js",
-    print: "./src/print.js",
+    app: './src/index.js'
   },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
   },
-  mode: "development",
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: "Output Management",
+      title: 'Hot Module Replacement'
     }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
   ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
