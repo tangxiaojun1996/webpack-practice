b2ff4c4e3d488a6f5e3025c300377634d177745a
“xj.tang” <xj.tang@wanwuyunlian.com>
2021-01-26 16:40:50 +0800

    chore

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-import _ from "lodash";
-import printMe from "./print.js";
-import './styles.css';
+import { cube } from "./math.js";
 
 function component() {
-  var element = document.createElement("div");
-  var btn = document.createElement("button");
+  var element = document.createElement("pre");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  btn.innerHTML = "Click me and check the console!!!";
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.innerHTML = [
+    "Hello webpack!",
+    "5 cubed is equal to 123123 " + cube(5),
+  ].join("\n\n");
 
   return element;
 }
 
 document.body.appendChild(component());
-
-if (module.hot) {
-  module.hot.accept("./print.js", function () {
-    console.log("Accepting the updated printMe module!");
-    printMe();
-  });
-}
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
+  mode: "development",
   entry: {
     app: "./src/index.js",
   },
